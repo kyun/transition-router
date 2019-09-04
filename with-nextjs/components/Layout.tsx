6623ea52f2007546bd7c9d8b1e97abe9bb9f828f
zynkn <wls5563@gmail.com>
2019-09-04 19:20:42 +0900

    cra 트랜지션

--- with-nextjs/components/Layout.tsx
+++ with-nextjs/components/Layout.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Head from 'next/Head';
-import Link from 'next/link';
+import Head from 'next/head';
+
 import { useRouter } from 'next/router';
 
 
@@ -13,11 +13,7 @@ const Layout: React.FC<any> = ({ title = "default title", children }) => {
         <title>{title}</title>
         <meta charSet="utf-8" />
       </Head>
-      <header>
-        <Link href="/"><a>Home</a></Link>
-        <Link href="/a"><a>a</a></Link>
-        <Link href="/b"><a>b</a></Link>
-      </header>
+      
       <main>
 
         {children}
@@ -26,30 +22,7 @@ const Layout: React.FC<any> = ({ title = "default title", children }) => {
       <footer>
 
       </footer>
-      <style jsx global>
-        {`
-          header > a{
-            margin: 16px;
-          }
-          .page-enter-active {
-            opacity: 1;
-            transform: scale(1);
-            transition: opacity 300ms, transform 300ms;
-          }
-          
-          .page-exit {
-            opacity: 1;
-            transform: scale(1);
-          }
-          
-          .page-exit-active {
-            opacity: 0;
-            transform: scale(0.9);
-            transition: opacity 300ms, transform 300ms;
-          }
-          
-        `}
-      </style>
+     
     </div>
 
   )
--- with-nextjs/pages/_app.tsx
+++ with-nextjs/pages/_app.tsx
@@ -1,5 +1,6 @@
 import App from 'next/app';
 import React from 'react';
+import Link from 'next/link';
 import { CSSTransition } from 'react-transition-group';
 
 const routes = ["/", "/a", "/b"];
@@ -9,12 +10,17 @@ export default class NextApp extends App {
     console.log(this.props.router.route)
     return (
       <div>
+        <header>
+        <Link href="/"><a>Home</a></Link>
+        <Link href="/a"><a>a</a></Link>
+        <Link href="/b"><a>b</a></Link>
+      </header>
         {
           routes.map(route => {
             return (
               <CSSTransition
                 key={route}
-                in={this.props.router.route == route}
+                in={this.props.router.route === route}
                 timeout={300}
                 classNames="page"
                 unmountOnExit
@@ -26,6 +32,53 @@ export default class NextApp extends App {
             )
           })
         }
+         <style jsx global>
+        {`
+        header{
+          width: 100%;
+          max-width: 768px;
+          margin: auto;
+        }
+          header > a{
+            
+            margin: 16px;
+            position: relative;
+            z-index: 99;
+          }
+          .pages{
+            position: fixed;
+            width: 100%;
+            max-width: 768px;
+            height: 100vh;
+            top: 56px;
+            left: 0;
+            right: 0;
+            background-color: rgba(0,0,0,0.1);
+            margin: auto;
+          }
+          .page-enter{
+            transform: translateX(-100px);
+            // transition: transform 300ms;
+          }
+          .page-enter-active {
+            opacity: 1;
+            transform: translateX(-0px);
+            transition: opacity 300ms, transform 300ms;
+          }
+          
+          .page-exit {
+            opacity: 1;
+            transform: translateX(0px);
+          }
+          
+          .page-exit-active {
+            opacity: 0;
+            transform: translateX(100px);
+            transition: opacity 300ms, transform 300ms;
+          }
+          
+        `}
+      </style>
       </div>
     )
   }
